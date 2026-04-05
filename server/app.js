c92e4814d0c062882cc32c9c1e649145e23f1076
Nikki Murello <nmurello15@gmail.com>
2026-04-05 18:21:36 -0400

    Finished part C

--- server/app.js
+++ server/app.js
@@ -6,6 +6,10 @@ const mongoose = require("mongoose");
 const expressHandlebars = require("express-handlebars");
 const helmet = require("helmet");
 const session = require("express-session");
+const { RedisStore } = require("connect-redis");
+const redis = require("redis");
+
+require("dotenv").config();
 
 const router = require("./router.js");
 
@@ -19,29 +23,40 @@ mongoose.connect(DB_URI).catch((err) => {
     }
 });
 
-const app = express();
+const redisClient = redis.createClient({
+    url: process.env.REDIS_URI
+});
 
-app.use(helmet());
-app.use("/assets", express.static(path.resolve(`${__dirname}/../hosted`)));
-app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
-app.use(compression());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+redisClient.on("error", (err) => console.log("Redis client error:", err));
 
-app.use(session({
-    key: "sessionid",
-    secret: "Domo Arigato",
-    resave: false,
-    saveUninitialized: false,
-}));
+redisClient.connect().then(() => {
+    const app = express();
 
-app.engine("handlebars", expressHandlebars.engine({ defaultLayout: "" }));
-app.set("view engine", "handlebars");
-app.set("views", `${__dirname}/../views`);
+    app.use(helmet());
+    app.use("/assets", express.static(path.resolve(`${__dirname}/../hosted`)));
+    app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+    app.use(compression());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
 
-router(app);
+    app.use(session({
+        key: "sessionid",
+        store: new RedisStore({
+            client: redisClient
+        }),
+        secret: "Domo Arigato",
+        resave: false,
+        saveUninitialized: false,
+    }));
 
-app.listen(PORT, (err) => {
-    if (err) throw err;
-    console.log(`Listening on 127.0.0.1:${PORT} !!`);
+    app.engine("handlebars", expressHandlebars.engine({ defaultLayout: "" }));
+    app.set("view engine", "handlebars");
+    app.set("views", `${__dirname}/../views`);
+
+    router(app);
+
+    app.listen(PORT, (err) => {
+        if (err) throw err;
+        console.log(`Listening on 127.0.0.1:${PORT} !!`);
+    });
 });
